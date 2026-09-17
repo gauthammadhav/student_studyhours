d75f25b8a78e1abb6fb91de4897cd30caec5f235
Gautham Madhav <145239326+gauthammadhav@users.noreply.github.com>
2026-09-17 11:02:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("student_result_prediction_model(1).pkl")
+model = joblib.load("logistic_regression_Student_StudyHrs_model.pkl")
 
-st.title("Student Pass/Fail based on Study Hours")
+st.title("Student Pass/Fail Prediction")
 
-hours = st.number_input("Enter Study Hours", min_value = 0.0, max_value = 15.0, value = 5.0)
+hours = st.number_input(
+    "Enter Study Hours",
+    min_value=0.0,
+    max_value=15.0,
+    value=5.0
+)
+
+attendance = st.number_input(
+    "Enter Attendance (%)",
+    min_value=0.0,
+    max_value=100.0,
+    value=75.0
+)
 
 if st.button("Predict"):
-  prediction = model.predict([[hours]])
+    prediction = model.predict([[hours, attendance]])
 
-  if prediction[0] == 1:
-    st.success("PASS")
-  else:
-    st.error("FAIL")
+    if prediction[0] == 1:
+        st.success("PASS 🎉")
+    else:
+        st.error("FAIL")
