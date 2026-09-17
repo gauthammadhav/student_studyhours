a48a97c872ad404466956495732196e7c256f57c
Gautham Madhav <145239326+gauthammadhav@users.noreply.github.com>
2026-09-17 10:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 
-model = joblib.load("logistic_regression_Student_StudyHrs_model.pkl")
+model = joblib.load("student_result_prediction_model.pkl")
 
 st.title("Student Pass/Fail based on Study Hours")
 
